87936a7fb6df5adf5115453b61b0de78d36cd79f
valesteb <estebanfullmusic@gmail.com>
2025-05-14 08:39:18 -0500

    Se agregan las instrucciones de la función para ir a un piso y se ejecutan en un bloque 'if__name__ = __main__' para testear

--- Elevator.py
+++ Elevator.py
@@ -4,4 +4,12 @@ Class Elevator:
     self.current_floor = 0
 
   def go_to_floor(self, floor):
-    
+    if self.current_floor == floor:
+      print(f"Elevator is in floor {floor} ")
+    else:
+      print(f"Going to floor {floor}")
+      self.current_floor = floor
+
+if __name__ = "__main__":
+  elevator = Elevator()
+  elevator.go_to_floor(3)
